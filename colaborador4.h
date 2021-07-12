08ee4f6261657351768606b2b83ef2f4288c72fc
Romario Vernaza <83619913+roma-27@users.noreply.github.com>
2021-07-12 16:20:29 -0500

    Modificación N°4
    Utilice el ciclo for para ingresar y presentar los datos la matriz 1, para realizar la transpuesta de la matriz, utilice otro ciclo for, esta vez para presentar la matriz pero las columnas pasaron a hacer filas y las filas pasaron a hacer las columnas

--- colaborador4.h
+++ colaborador4.h
@@ -1,9 +1,37 @@
-void transpuesta(float (*m1)[5], float (*r)[5])
+#include<iostream>
+using namespace std;
+void transpuesta(float (*m1)[5], float (*r)[5]);
+
+int main()
 {
-
-cout<<"Obtener la transpuesta de una matriz";
-
-
+int i,j;
+float matriz1[5][5];
+cout<<"Obtener la transpuesta de una matriz \n";
+cout<<"Ingrese los datos de la matriz1 \n";
+	for(i=0;i<5;i++)
+      {
+        for(j=0;j<5;j++)
+        {
+            cout<<"\nPosicion["<<i<<"]["<<j<<"]=  ";
+            cin>>matriz1[i][j];
+        }
+      } 
+      cout<<"\nLos datos de la matriz1 son:\n";
+      for(i=0;i<5;i++)
+      {
+        for(j=0;j<5;j++)
+        {
+            cout<<"\t"<<matriz1[i][j];
+        }
+        cout<<endl;
+      }
+cout<<"La transpuesta de la matriz1 es: \n";
+for(i=0;i<5;i++){
+		for(j=0;j<5;j++){
+			cout<<matriz1[j][i]<<" ";
+		}
+		cout<<"\n";	
+	}
 
 
 }

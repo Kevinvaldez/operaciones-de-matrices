9e7bd6353d9a8eb0b3e707097e4525b833b97525
Kevin Ismael Valdez Mendoza <86176535+Kevinvaldez@users.noreply.github.com>
2021-07-12 15:17:26 -0500

    Modificación N°1
    Para esta modificación utilicé el ciclo de repetición for, para presentar los valores de la matriz1 y 2

--- colaborador1.h
+++ colaborador1.h
@@ -1,9 +1,53 @@
-void ingresar(float (*m1)[5], float (*m2)[5])
+#include<iostream>
+using namespace std;
+void ingresar(float (*m1)[5], float (*m2)[5]);
+
+int main()
 {
-
-cout<<"Ingresar y mostrar los valores de los arreglos o matrices";
-
+int i,j;
+float matriz1[5][5],matriz2[5][5];
+cout<<"Ingresar y mostrar los valores de los arreglos o matrices \n";
+	//Matriz 1
+	cout<<"Ingrese los datos de la matriz1"<<endl;
+	for(i=0;i<5;i++)
+      {
+        for(j=0;j<5;j++)
+        {
+            cout<<"\nPosicion["<<i<<"]["<<j<<"]=  ";
+            cin>>matriz1[i][j];
+        }
+      } 
+      cout<<"\nLos datos de la matriz1 son:\n";
+      for(i=0;i<5;i++)
+      {
+        for(j=0;j<5;j++)
+        {
+            cout<<"\t"<<matriz1[i][j];
+        }
+        cout<<endl;
+      }
+      //Matriz2
+      cout<<"ingrese datos de la matriz2\n";
+      for(i=0;i<5;i++)
+      {
+        for(j=0;j<5;j++)
+        {
+            cout<<"\nPosicion["<<i<<"]["<<j<<"]=  ";
+            cin>>matriz2[i][j];
+        }
+      } 
+       
+      cout<<"\nLos datos de la matriz2 son:\n";
+      for(i=0;i<5;i++)
+      {
+        for(j=0;j<5;j++)
+        {
+            cout<<"\t"<<matriz2[i][j];
+        }
+        cout<<endl;
+      }
 
 
 
 }
+

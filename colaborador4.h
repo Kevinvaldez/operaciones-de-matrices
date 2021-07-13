6d2d5e5faccc385168feb8602a0f999a8c1e847e
Romario Vernaza <83619913+roma-27@users.noreply.github.com>
2021-07-13 11:34:10 -0500

    Update colaborador4.h

--- colaborador4.h
+++ colaborador4.h
@@ -1,30 +1,4 @@
-#include<iostream>
-using namespace std;
-void transpuesta(float (*m1)[5], float (*r)[5]);
 
-int main()
-{
-int i,j;
-float matriz1[5][5];
-cout<<"Obtener la transpuesta de una matriz \n";
-cout<<"Ingrese los datos de la matriz1 \n";
-	for(i=0;i<5;i++)
-      {
-        for(j=0;j<5;j++)
-        {
-            cout<<"\nPosicion["<<i<<"]["<<j<<"]=  ";
-            cin>>matriz1[i][j];
-        }
-      } 
-      cout<<"\nLos datos de la matriz1 son:\n";
-      for(i=0;i<5;i++)
-      {
-        for(j=0;j<5;j++)
-        {
-            cout<<"\t"<<matriz1[i][j];
-        }
-        cout<<endl;
-      }
 cout<<"La transpuesta de la matriz1 es: \n";
 for(i=0;i<5;i++){
 		for(j=0;j<5;j++){
@@ -34,4 +8,4 @@ for(i=0;i<5;i++){
 	}
 
 
-}
+
